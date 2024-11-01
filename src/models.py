5d1c0ce544adabc2ece71a44145dfee09a05670c
Joaquin Morales <Mintinvestments95@gmail.com>
2024-11-01 23:40:41 +0000

    Need line 48

--- src/models.py
+++ src/models.py
@@ -7,26 +7,60 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+         
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
+    followers = relationship("Follower", foreign_keys="[Follower.user_to_id]", back_populates="user_to")
+    following = relationship("Follower", foreign_keys="[Follower.user_from_id]", back_populates="user_from")
+    favorites = relationship("Favorites", back_populates="user") 
+
+class Post(Base):
+     __tablename__ = 'post'
+     id = Column(Integer, primary_key=True)
+     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+     user = relationship("User", back_populates="posts")
+     comments = relationship("Comment", back_populates="post")
+     favorited_by = relationship("Favorites", back_populates="post")
+    
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)  
+
+    user_from = relationship("User", foreign_keys=[user_from_id], back_populates="following")
+    user_to = relationship("User", foreign_keys=[user_to_id], back_populates="followers")
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    comment_text = Column(Text, nullable=False)
+
+    post = relationship("Post", back_populates="comments")
+    user = relationship("User", back_populates="comments")
 
     def to_dict(self):
         return {}
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    user = relationship("User", back_populates="favorites")
+    post = relationship("Post", back_populates="favorited_by")
 
 ## Draw from SQLAlchemy base
 try:
